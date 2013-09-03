d9b140471c0eefc0292a546bfff87921e2088dc8
Maan Bsat <maan@supermaan.com>
2013-09-03 17:34:25 -0400

    Clean up directory structure and use distutils

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+setup.py
+
+Created by Maan Bsat on 2013-09-03.
+Copyright (c) 2013 Maan Bsat. All rights reserved.
+"""
+
+from distutils.core import setup
+
+setup(
+    name='untws',
+    version='0.1',
+    packages=['untws'],
+)
